ec789c7095399c29363d353a54ed24134fbbfb64
Oele Geirnaert <oelegeirnaert@hotmail.com>
2025-02-07 12:03:57 +0100

    shortcuts

---
+++ aliasses.sh
@@ -0,0 +1,6 @@
+# source alliasses.sh
+alias devup='docker compose -f docker-compose.local.yml up'
+alias devtest='docker compose -f docker-compose.local.yml run --rm django coverage run -m pytest'
+alias mm='docker compose -f docker-compose.local.yml run --rm django python manage.py makemigrations'
+alias mi='docker compose -f docker-compose.local.yml run --rm django python manage.py migrate'
+alias manage='docker compose -f docker-compose.local.yml run --rm django python manage.py'
