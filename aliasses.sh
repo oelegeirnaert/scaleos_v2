a556969c60ee71af4b12e34bba30e48ea294f1fb
Oele Geirnaert <oelegeirnaert@hotmail.com>
2025-02-07 13:41:10 +0100

    updated aliasses

--- aliasses.sh
+++ aliasses.sh
@@ -1,5 +1,6 @@
-# source alliasses.sh
+# source aliasses.sh
 alias devup='docker compose -f docker-compose.local.yml up'
+alias devbuild='docker compose -f docker-compose.local.yml build'
 alias devtest='docker compose -f docker-compose.local.yml run --rm django coverage run -m pytest'
 alias mm='docker compose -f docker-compose.local.yml run --rm django python manage.py makemigrations'
 alias mi='docker compose -f docker-compose.local.yml run --rm django python manage.py migrate'
