352d6d51e423bd91b7c5cf9d2fa137effda56556
Oele Geirnaert <oelegeirnaert@hotmail.com>
2025-02-08 10:36:49 +0100

    cleanup + adding of documentation shortcuts

--- aliasses.sh
+++ aliasses.sh
@@ -1,11 +1,19 @@
 # source aliasses.sh
 alias devup='docker compose -f docker-compose.local.yml up'
 alias devbuild='docker compose -f docker-compose.local.yml build'
-alias devtest='docker compose -f docker-compose.local.yml run --rm django coverage run -m pytest'
-alias testreport='devtest && docker compose -f docker-compose.local.yml run --rm django coverage html'
 alias mm='docker compose -f docker-compose.local.yml run --rm django python manage.py makemigrations'
 alias mi='docker compose -f docker-compose.local.yml run --rm django python manage.py migrate'
 alias devshell='docker compose -f docker-compose.local.yml run --rm django python manage.py shell_plus --ipython'
 alias manage='docker compose -f docker-compose.local.yml run --rm django python manage.py'
+
+# Documentation
+alias docsup="docker compose -f docker-compose.local.yml -f docker-compose.docs.yml up"
+alias docsopen="chromium-browser http://localhost:9000/"
+
+# Testing
+alias devtest='docker compose -f docker-compose.local.yml run --rm django coverage run -m pytest'
+alias testreport='devtest && docker compose -f docker-compose.local.yml run --rm django coverage html'
+
+# Data
 alias mkwaerboom='docker compose -f docker-compose.local.yml run --rm django python manage.py create_organization waerboom'
 alias mkpersons='docker compose -f docker-compose.local.yml run --rm django python manage.py create_persons'
