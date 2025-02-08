ef1bbcee910c6a27410f69c36ef1609c1498c13b
Oele Geirnaert <oelegeirnaert@hotmail.com>
2025-02-08 10:41:42 +0100

    consequences of polymorph model pricematrix

--- scaleos/reservations/models.py
+++ scaleos/reservations/models.py
@@ -19,17 +19,17 @@ class Reservation(PolymorphicModel):
         verbose_name_plural = _("reservations")   
 
 class BrunchReservation(Reservation):
-    brunch = models.ForeignKey("events.BrunchEvent", related_name="reservations", on_delete=models.CASCADE, null=True, blank=False)
+    brunch_event = models.ForeignKey("events.BrunchEvent", related_name="reservations", on_delete=models.CASCADE, null=True, blank=False)
     amount = models.IntegerField(null=True, blank=False)
-    price = models.ForeignKey('payments.AgePriceMatrixItem', on_delete=models.SET_NULL, null=True, blank=False)
+    age_price_matrix_item = models.ForeignKey('payments.AgePriceMatrixItem', on_delete=models.SET_NULL, null=True, blank=False)
 
     @property
     def total_price(self):
 
         if self.amount is None:
             return 0
-        if self.price:
-            return self.amount * self.price.price.current_price.amount
+        if self.age_price_matrix_item:
+            return self.amount * self.age_price_matrix_item.price.current_price.amount
         
         return self.price
     
