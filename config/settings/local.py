585f76a56ad428c79cf3187b81f7c91eb7799591
Oele Geirnaert <oelegeirnaert@hotmail.com>
2025-02-25 20:47:15 +0100

    allow tailscale

--- config/settings/local.py
+++ config/settings/local.py
@@ -23,7 +23,7 @@ SECRET_KEY = env(
     default="8Y50FknlugcaQRQ6nzxALcLxsy9i3d7LbjxvSawzpr57qwodYXPwEK95Lz7ppbbi",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]  # noqa: S104
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django", ".drum-vimba.ts.net"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
