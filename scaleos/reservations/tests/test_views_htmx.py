2840284d66fb577edfae23b32565279cd4cf921d
Oele Geirnaert <oelegeirnaert@hotmail.com>
2025-02-26 10:49:37 +0100

    100percent tested

--- scaleos/reservations/tests/test_views_htmx.py
+++ scaleos/reservations/tests/test_views_htmx.py
@@ -5,6 +5,7 @@ from django.core import mail
 from django.urls import reverse
 
 from scaleos.events.tests import model_factories as event_factories
+from scaleos.payments.tests import model_factories as payment_factories
 from scaleos.reservations import models as reservation_models
 from scaleos.reservations.tests import model_factories as reservation_factories
 from scaleos.users.tests import model_factories as user_factories
@@ -99,9 +100,32 @@ def test_htmx_finish_reservation_reservation_is_successfull_as_authenticated(cli
 
 
 @pytest.mark.django_db
-def test_htmx_get_event_reservation(client):
+def test_htmx_event_reservation_without_pricing(client):
     a_uuid = "c221ac76-3829-4e3d-975a-3504e3332ccc"
     event_factories.EventFactory(public_key=a_uuid)
+    headers = {"HTTP_HX-Request": "true"}
+    url = reverse(
+        "reservations_htmx:event_reservation",
+        kwargs={"event_public_key": a_uuid},
+    )
+    response = client.get(url, **headers)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_htmx_event_reservation_with_pricing(client):
+    age_price_matrix = payment_factories.AgePriceMatrixFactory()
+    payment_factories.AgePriceMatrixItemFactory(
+        age_price_matrix_id=age_price_matrix.pk,
+    )
+    concept = event_factories.ConceptFactory()
+    event_factories.ConceptPriceMatrixFactory(
+        concept_id=concept.pk,
+        price_matrix_id=age_price_matrix.pk,
+    )
+
+    a_uuid = "c221ac76-3829-4e3d-975a-3504e3332ccc"
+    event_factories.EventFactory(public_key=a_uuid, concept_id=concept.pk)
     headers = {"HTTP_HX-Request": "true"}
     url = reverse(
         "reservations_htmx:event_reservation",
